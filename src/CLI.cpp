2859b33ac4fbdee2eb1a31320426bf498cfa0ef2
Mois-Espinal <154764168+Mois-Espinal@users.noreply.github.com>
2024-02-03 15:35:33 -0500

    Add to CLI.cpp and create the manual

--- src/CLI.cpp
+++ src/CLI.cpp
@@ -1,8 +1,21 @@
  //Command Line Interface VERSION 
  #include <iostream>
- #include <unistd.h>
+ #include <fstream>
+ // Hashing function 
+ int hash(std::string const& inputString){
+     if (inputString == "help") return 1;
+ }
  // Add command arguments when executing the file later
  int main(int argc, char *argv[]){
+ std::string CommandName = argv[1];
+ int hashedCommand = hash(CommandName)
+ int params;
+ switch(hashedCommand){
+ case 1:
  
+ break;
+ default:
+  std::cout << "Unknown command, maybe type light-todo -help";
+ }
  return 0;
  }
