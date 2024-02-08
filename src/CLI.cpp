fc97b3b6d947daa59028b20ad2eeffad5ab78dbf
Mois-Espinal <154764168+Mois-Espinal@users.noreply.github.com>
2024-02-08 18:56:03 -0500

    Add 2 useable commands and fill out manual.txt

--- src/CLI.cpp
+++ src/CLI.cpp
@@ -3,17 +3,48 @@
  #include <fstream>
  // Hashing function 
  int hash(std::string const& inputString){
-     if (inputString == "help") return 1;
+    if (inputString == "add") return 1;
+    if (inputString == "due") return 2;
+    if (inputString == "help") return 3;
+    if (inputString == "list") return 4;
+    if (inputString == "delete") return 5;
+    if (inputString == "complete") return 6;
+    if (inputString == "prioritize") return 7;
+   
+   return 0;
  }
  // Add command arguments when executing the file later
  int main(int argc, char *argv[]){
  std::string CommandName = argv[1];
- int hashedCommand = hash(CommandName)
+ int hashedCommand = hash(CommandName);
  int params;
- switch(hashedCommand){
+ switch(hashedCommand){ 
+    //ADD COMMAND
+    std::ifstream todoList ("list.txt");
+    if (todoList.is_open())
+      todoList << "\n" << argv[2];
  case 1:
+ {
+
  
  break;
+ }
+ //HELP COMMAND
+ case 3:
+ {
+ std::ifstream helpPage ("manual.txt");
+  if (helpPage.is_open())
+        std::cout << helpPage.rdbuf();
+ break;
+ }
+ //LIST COMMAND
+ case 4:
+  {
+ std::ifstream todoList ("list.txt");
+  if (todoList.is_open())
+        std::cout << todoList.rdbuf();
+ break;
+ }
  default:
   std::cout << "Unknown command, maybe type light-todo -help";
  }
